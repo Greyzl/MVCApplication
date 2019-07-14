d237fce90149717a2bd81c572b4cafc32aa293f8
Sergey Shilkov <sergey_s96@mail.ru>
2019-07-14 14:17:28 +0300

    Add example with value injection

--- src/main/java/com/flippedMadTracker/Task/DeclareSpringComponents.java
+++ src/main/java/com/flippedMadTracker/Task/DeclareSpringComponents.java
@@ -4,11 +4,11 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class DeclareSpringComponents {
     public static void main(String[] args) {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("app-context-annotation.xml");
-        ctx.refresh();
-        MessageRenderer messageRenderer = ctx.getBean("renderer",MessageRenderer.class);
-        messageRenderer.render();
-        ctx.close();
+//        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+//        ctx.load("app-context-annotation.xml");
+//        ctx.refresh();
+//        //MessageRenderer messageRenderer = ctx.getBean("renderer",MessageRenderer.class);
+//        MessageProvider provider = ctx.getBean("provider",MessageProvider.class);
+//        System.out.println(provider.getMessage());
     }
 }
