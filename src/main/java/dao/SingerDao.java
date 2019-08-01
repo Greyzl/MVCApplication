9563f74a626e7a7a26bf168bba85a820e3da6329
Sergey Shilkov <sergey_s96@mail.ru>
2019-08-01 19:33:55 +0300

    jdbc

---
+++ src/main/java/dao/SingerDao.java
@@ -0,0 +1,17 @@
+package dao;
+
+import entity.Singer;
+
+import java.util.List;
+
+public interface SingerDao {
+    List<Singer> findAll();
+//    List<Singer> findAllByFirstName(String firstName);
+//    String findLastNameById(Long id);
+//    String findFirstNameByID(Long id);
+    void insert (Singer singer);
+//    void update (Singer singer);
+//    void delete (Long id);
+//    List<Singer> findAllWithDetail();
+//    void insertWithDetail(Singer singer);
+}
